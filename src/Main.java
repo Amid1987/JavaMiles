b322a9338ba768ddaea81040359640ac44c7f868
Dmitrii <fsb2100_1987@mail.ru>
2022-09-09 21:50:07 +0300

    JavaMiles

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15_850;
+        int miles = ticketPrice / 20;
+        int bonusMiles = miles;
+
+        System.out.println("Начислено миль: " + bonusMiles);
+
+
+    }
+}
